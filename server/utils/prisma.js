2ac10fba5b671c75bbdbce650e9c83d1e216c061
i-sohail <i-sohail@users.noreply.github.com>
2026-05-09 00:27:26 +0530

    fix: alias id to _id in Prisma for frontend compatibility

--- server/utils/prisma.js
+++ server/utils/prisma.js
@@ -1,6 +1,17 @@
 // server/utils/prisma.js
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  result: {
+    $allModels: {
+      _id: {
+        needs: { id: true },
+        compute(model) {
+          return model.id;
+        },
+      },
+    },
+  },
+});
 
 module.exports = prisma;
